c88f73493c895f736eb95ffa030e6b5389667468
XXOP71 <jhb0b@naver.com>
2019-11-12 00:35:41 +0900

    191112-1

---
+++ C07MTools/C07MTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+
+namespace C07MTools
+{
+    public static class MArr
+    {
+        public static bool Yes(ICollection tarr)
+        {
+            return (tarr != null) && (tarr.Count > 0);
+        }
+    }
+}
